f267b8ccc7b9b515f2561bfe3d9fb4a68bb7c824
ptaushanov <pepi15032000@gmail.com>
2022-05-19 20:04:52 +0300

    feat: added EF connection

---
+++ LibraryWPF/DAL/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryWPF.DAL
+{
+    internal class LibraryContext : DbContext
+    {
+        // TODO add DBSet<???>
+        public LibraryContext() : base() { }
+    }
+}
