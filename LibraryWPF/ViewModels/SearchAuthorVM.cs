0e14683d20bb75128cffe99af4a6ee80afc63f75
ptaushanov <pepi15032000@gmail.com>
2022-06-09 16:10:34 +0300

    feat: selectable suggestions

--- LibraryWPF/ViewModels/SearchAuthorVM.cs
+++ LibraryWPF/ViewModels/SearchAuthorVM.cs
@@ -23,22 +23,22 @@ namespace LibraryWPF.ViewModels
         private Author _selectedAuthor;
         private string _selectedCategory;
         private bool _popupOpen;
-        private TextBox _selectedTextBox;
 
         public RelayCommand ChangeToSearchControlCommand { get; private set; }
         public RelayCommand ChangeToResultsControlCommand { get; private set; }
-        public RelayCommand FocusChangedCommand { get; private set; }
+        public RelayCommand NextListBoxSelectedIndexCommand { get; private set; }
+        public RelayCommand PreviousListBoxSelectedIndexCommand { get; private set; }
 
         public SearchAuthorVM()
         {
             ChangeToSearchControlCommand = new RelayCommand(ChangeToSearchControl);
             ChangeToResultsControlCommand = new RelayCommand(ChangeToResultsControl);
-            FocusChangedCommand = new RelayCommand(FocusChanged);
+            NextListBoxSelectedIndexCommand = new RelayCommand(NextListBoxSelectedIndex);
+            PreviousListBoxSelectedIndexCommand = new RelayCommand(PreviousListBoxSelectedIndex);
             CurrentControl = new SearchAuthorControl();
-
             Suggestions = new ObservableCollection<string>();
             SelectedAuthor = null;
-            SelectedTextBox = null;
+            CanSuggest = true;
         }
 
         private void PropChanged(string propertyName)
@@ -67,6 +67,7 @@ namespace LibraryWPF.ViewModels
             get { return _title; }
             set
             {
+                if (value == null) { return; }
                 _title = value;
                 TrySuggestAuthor("Title");
                 PropChanged("Title");
@@ -84,6 +85,7 @@ namespace LibraryWPF.ViewModels
             get { return _firstName; }
             set
             {
+                if (value == null) { return; }
                 _firstName = value;
                 TrySuggestAuthor("FirstName");
                 PropChanged("FirstName");
@@ -95,6 +97,7 @@ namespace LibraryWPF.ViewModels
             get { return _lastName; }
             set
             {
+                if (value == null) { return; }
                 _lastName = value;
                 TrySuggestAuthor("LastName");
                 PropChanged("LastName");
@@ -106,6 +109,7 @@ namespace LibraryWPF.ViewModels
             get { return _publisher; }
             set
             {
+                if (value == null) { return; }
                 _publisher = value;
                 TrySuggestAuthor("Publisher");
                 PropChanged("Publisher");
@@ -151,18 +155,10 @@ namespace LibraryWPF.ViewModels
             }
         }
 
-        public TextBox SelectedTextBox
-        {
-            get { return _selectedTextBox; }
-            set
-            {
-                _selectedTextBox = value;
-                PropChanged("SelectedTextBox");
-            }
-        }
-
+        public bool CanSuggest { get; set; }
         private void TrySuggestAuthor(string propertyName)
         {
+            if (!CanSuggest) { return; }
             SuggestionsManager.SwitchContext(this, propertyName);
             SuggestionsManager.Suggest<Author, string>(this, Suggestions, true);
             PopupOpen = Suggestions.Count > 0;
@@ -198,10 +194,27 @@ namespace LibraryWPF.ViewModels
             CurrentControl = new SearchResultsControl();
         }
 
-        private void FocusChanged(object textField)
+        private void NextListBoxSelectedIndex(object _listbox)
         {
-            SelectedTextBox = textField as TextBox;
+            ListBox listBox = _listbox as ListBox;
+
+            if (!listBox.IsFocused)
+            {
+                listBox.Focus();
+                listBox.SelectedIndex = (listBox.SelectedIndex + 1) % listBox.Items.Count;
+            }
         }
 
+        private void PreviousListBoxSelectedIndex(object _listbox)
+        {
+            ListBox listBox = _listbox as ListBox;
+
+            if (!listBox.IsFocused)
+            {
+                listBox.Focus();
+                int previousIndex = (listBox.SelectedIndex - 1);
+                listBox.SelectedIndex = previousIndex < 0 ? listBox.Items.Count - 1 : previousIndex;
+            }
+        }
     }
 }
