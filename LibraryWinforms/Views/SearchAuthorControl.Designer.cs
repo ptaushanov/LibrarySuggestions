584f23adaafb2ada48aebaead715b27bf9219e17
ptaushanov <pepi15032000@gmail.com>
2022-06-16 19:16:37 +0300

    feat: winforms search author implementation (part 1)

--- LibraryWinforms/Views/SearchAuthorControl.Designer.cs
+++ LibraryWinforms/Views/SearchAuthorControl.Designer.cs
@@ -38,8 +38,8 @@
             this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.TitleTextBox = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.SuggestionsListBox = new System.Windows.Forms.ListBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,7 +57,8 @@
             this.tableLayoutPanel1.Controls.Add(this.label5, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.button1, 1, 4);
             this.tableLayoutPanel1.Controls.Add(this.TitleTextBox, 1, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(79, 108);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(225, 199);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 5;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
@@ -65,32 +66,44 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(318, 186);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(583, 343);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // PublisherTextBox
             // 
+            this.PublisherTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.PublisherTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.PublisherTextBox.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PublisherTextBox.Location = new System.Drawing.Point(81, 114);
+            this.PublisherTextBox.Location = new System.Drawing.Point(135, 210);
+            this.PublisherTextBox.Margin = new System.Windows.Forms.Padding(6);
             this.PublisherTextBox.Name = "PublisherTextBox";
-            this.PublisherTextBox.Size = new System.Drawing.Size(233, 23);
+            this.PublisherTextBox.Size = new System.Drawing.Size(424, 34);
             this.PublisherTextBox.TabIndex = 15;
+            this.PublisherTextBox.Enter += new System.EventHandler(this.HandleTextBoxEnter);
             // 
             // LastNameTextBox
             // 
+            this.LastNameTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.LastNameTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.LastNameTextBox.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LastNameTextBox.Location = new System.Drawing.Point(81, 77);
+            this.LastNameTextBox.Location = new System.Drawing.Point(135, 142);
+            this.LastNameTextBox.Margin = new System.Windows.Forms.Padding(6);
             this.LastNameTextBox.Name = "LastNameTextBox";
-            this.LastNameTextBox.Size = new System.Drawing.Size(233, 23);
+            this.LastNameTextBox.Size = new System.Drawing.Size(424, 34);
             this.LastNameTextBox.TabIndex = 14;
+            this.LastNameTextBox.Enter += new System.EventHandler(this.HandleTextBoxEnter);
             // 
             // FirstNameTextBox
             // 
+            this.FirstNameTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.FirstNameTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.FirstNameTextBox.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FirstNameTextBox.Location = new System.Drawing.Point(81, 40);
+            this.FirstNameTextBox.Location = new System.Drawing.Point(135, 74);
+            this.FirstNameTextBox.Margin = new System.Windows.Forms.Padding(6);
             this.FirstNameTextBox.Name = "FirstNameTextBox";
-            this.FirstNameTextBox.Size = new System.Drawing.Size(233, 23);
+            this.FirstNameTextBox.Size = new System.Drawing.Size(424, 34);
             this.FirstNameTextBox.TabIndex = 13;
+            this.FirstNameTextBox.Enter += new System.EventHandler(this.HandleTextBoxEnter);
             // 
             // label1
             // 
@@ -99,9 +112,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Location = new System.Drawing.Point(6, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 37);
+            this.label1.Size = new System.Drawing.Size(117, 68);
             this.label1.TabIndex = 0;
             this.label1.Text = "Заглавие";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -113,9 +127,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(3, 37);
+            this.label3.Location = new System.Drawing.Point(6, 68);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 37);
+            this.label3.Size = new System.Drawing.Size(117, 68);
             this.label3.TabIndex = 2;
             this.label3.Text = "Име";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -127,9 +142,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(3, 74);
+            this.label4.Location = new System.Drawing.Point(6, 136);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 37);
+            this.label4.Size = new System.Drawing.Size(117, 68);
             this.label4.TabIndex = 3;
             this.label4.Text = "Фамилия";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -141,9 +157,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(3, 111);
+            this.label5.Location = new System.Drawing.Point(6, 204);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(72, 37);
+            this.label5.Size = new System.Drawing.Size(117, 68);
             this.label5.TabIndex = 4;
             this.label5.Text = "Издател";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -153,10 +170,11 @@
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.button1.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(205, 155);
+            this.button1.Location = new System.Drawing.Point(375, 285);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
             this.button1.Name = "button1";
-            this.button1.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.button1.Size = new System.Drawing.Size(110, 28);
+            this.button1.Padding = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.button1.Size = new System.Drawing.Size(202, 52);
             this.button1.TabIndex = 10;
             this.button1.Tag = "SaveSuggestionCommand";
             this.button1.Text = "Търси";
@@ -164,41 +182,48 @@
             // 
             // TitleTextBox
             // 
+            this.TitleTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.TitleTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.TitleTextBox.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TitleTextBox.Location = new System.Drawing.Point(81, 3);
+            this.TitleTextBox.Location = new System.Drawing.Point(135, 6);
+            this.TitleTextBox.Margin = new System.Windows.Forms.Padding(6);
             this.TitleTextBox.Name = "TitleTextBox";
-            this.TitleTextBox.Size = new System.Drawing.Size(233, 23);
+            this.TitleTextBox.Size = new System.Drawing.Size(424, 34);
             this.TitleTextBox.TabIndex = 12;
-            // 
-            // listBox1
-            // 
-            this.listBox1.BackColor = System.Drawing.Color.White;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(432, 108);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(165, 186);
-            this.listBox1.TabIndex = 2;
+            this.TitleTextBox.Enter += new System.EventHandler(this.HandleTextBoxEnter);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Cascadia Code", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(79, 62);
+            this.label2.Location = new System.Drawing.Point(225, 114);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 28);
+            this.label2.Size = new System.Drawing.Size(176, 49);
             this.label2.TabIndex = 3;
             this.label2.Text = "Търсене";
             // 
+            // SuggestionsListBox
+            // 
+            this.SuggestionsListBox.FormattingEnabled = true;
+            this.SuggestionsListBox.ItemHeight = 24;
+            this.SuggestionsListBox.Location = new System.Drawing.Point(888, 199);
+            this.SuggestionsListBox.Name = "SuggestionsListBox";
+            this.SuggestionsListBox.Size = new System.Drawing.Size(265, 340);
+            this.SuggestionsListBox.TabIndex = 4;
+            this.SuggestionsListBox.SelectedValueChanged += new System.EventHandler(this.HandleSelectedValueChanged);
+            // 
             // SearchAuthorControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.SuggestionsListBox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "SearchAuthorControl";
-            this.Size = new System.Drawing.Size(700, 400);
+            this.Size = new System.Drawing.Size(1283, 738);
             this.Load += new System.EventHandler(this.HandleControlLoad);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
@@ -219,7 +244,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox TitleTextBox;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox SuggestionsListBox;
     }
 }
--- LibraryWinforms/Views/SearchAuthorControl.cs
+++ LibraryWinforms/Views/SearchAuthorControl.cs
@@ -14,15 +14,47 @@ namespace LibraryWinforms.Views
     public partial class SearchAuthorControl : UserControl
     {
         public SearchAuthorVM SearchAuthorViewModel { get; private set; }
+        private TextBox _focusedTextBox;
         public SearchAuthorControl(SearchAuthorVM searchAuthorVM)
         {
             SearchAuthorViewModel = searchAuthorVM;
+            _hasSelectedSuggestion = false;
             InitializeComponent();
         }
 
         private void HandleControlLoad(object sender, EventArgs e)
         {
+            SuggestionsListBox
+                .DataBindings
+                .Add(new Binding("DataSource", SearchAuthorViewModel, "Suggestions", true, DataSourceUpdateMode.Never));
 
+            TitleTextBox
+                .DataBindings
+                .Add(new Binding("Text", SearchAuthorViewModel, "Title", true, DataSourceUpdateMode.OnPropertyChanged));
+
+            FirstNameTextBox
+                .DataBindings
+                .Add(new Binding("Text", SearchAuthorViewModel, "FirstName", true, DataSourceUpdateMode.OnPropertyChanged));
+
+            LastNameTextBox
+                .DataBindings
+                .Add(new Binding("Text", SearchAuthorViewModel, "LastName", true, DataSourceUpdateMode.OnPropertyChanged));
+
+            PublisherTextBox
+                .DataBindings
+                .Add(new Binding("Text", SearchAuthorViewModel, "Publisher", true, DataSourceUpdateMode.OnPropertyChanged));
+        }
+
+        private void HandleTextBoxEnter(object sender, EventArgs e)
+        {
+            _focusedTextBox = sender as TextBox;
+            SearchAuthorViewModel.CanSuggest = true;
+        }
+
+        private void HandleSelectedValueChanged(object sender, EventArgs e)
+        {
+            _focusedTextBox.Text = (sender as ListBox).SelectedItem as string;
+            SearchAuthorViewModel.CanSuggest = false;
         }
     }
 }
